a593b97ccf9c05e24624a376b62b64144a051383
Sridhar <sridharsundar100@gmail.com>
2025-08-24 17:41:06 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,7 +100,8 @@ DATABASES = {
         
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PWD"),
